9414ad05f409bd0365f51203755655488b69d87e
Gary Smith <bk@kc.gs>
2018-02-17 13:29:29 -0500

    move rp into functions
    Signed-off-by: Gary Smith <bk@kc.gs>

--- pclookup.js
+++ pclookup.js
@@ -25,44 +25,61 @@ function auth (context) {
   return 'authorized';
 }
 
+function lookupPc (context, callback) {
+  var postalcode = context.data.postalcode;
+  var country = context.data.country;
+  var geonames = context.secrets.GEONAME;
+
+  var postalCodeLookup = 'http://api.geonames.org/postalCodeLookupJSON?postalcode=' + postalcode + '&country=' + country + '&username=' + geonames;
+
+  console.log(postalCodeLookup);
+  var getPC = {
+    uri: postalCodeLookup,
+    json: true
+  };
+
+  rp(getPC)
+    .then(function (response) {
+      getTimezone(context, callback, response);
+    })
+    .catch(function (err) {
+      callback(null, err);
+    });
+}
+
+function getTimezone (context, callback, places) {
+  var geonames = context.secrets.GEONAME;
+  var tzURL = 'http://api.geonames.org/timezoneJSON?username=' + geonames;
+
+  tzURL = tzURL + '&lat=' + places.postalcodes[0].lat + '&lng=' + places.postalcodes[0].lng;
+  console.log(tzURL);
+  rp(tzURL, {uri: tzURL, json: true})
+    .then(function (tzresponse) {
+      for (var place of places.postalcodes) {
+        place.countryName = tzresponse.countryName;
+        place.timezoneId = tzresponse.timezoneId;
+      }
+      return places;
+    })
+    .catch(function (err) {
+      callback(null, err);
+    });
+}
+
 module.exports =
   function (context, callback) {
-    var postalcode = context.data.postalcode;
-    var country = context.data.country;
-    var geonames = context.secrets.GEONAME;
-
     var isAuth = auth(context);
+
     console.log(isAuth);
     if (isAuth !== 'authorized') {
       callback(null, isAuth);
     }
 
-    var postalCodeLookup = 'http://api.geonames.org/postalCodeLookupJSON?postalcode=' + postalcode + '&country=' + country + '&username=' + geonames;
-    var tzURL = 'http://api.geonames.org/timezoneJSON?username=' + geonames;
-    console.log(postalCodeLookup);
-    var getPC = {
-      uri: postalCodeLookup,
-      json: true
-    };
+    let places = lookupPc(context, callback);
+    // console.log(places);
 
-    rp(getPC)
-      .then(function (response) {
-        var places = response;
-        tzURL = tzURL + '&lat=' + places.postalcodes[0].lat + '&lng=' + places.postalcodes[0].lng;
-        console.log(tzURL);
-        rp(tzURL, {uri: tzURL, json: true})
-          .then(function (tzresponse) {
-            for (var place of places.postalcodes) {
-              place.countryName = tzresponse.countryName;
-              place.timezoneId = tzresponse.timezoneId;
-            }
-            callback(null, places);
-          })
-          .catch(function (err) {
-            callback(null, err);
-          });
-      })
-      .catch(function (err) {
-        callback(null, err);
-      });
+    // let tz = getTimezone(context, callback, places);
+    // console.log(tz);
+
+    callback(null, places);
   };
